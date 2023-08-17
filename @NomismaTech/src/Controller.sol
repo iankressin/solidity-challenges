2bec8f2456d61016b93be4d9a0f530404d754910
Ian Guimaraes <ianguimaraes31@gmail.com>
2023-08-17 18:30:00 -0300

    feat: sync

--- @NomismaTech/src/Controller.sol
+++ @NomismaTech/src/Controller.sol
@@ -12,22 +12,12 @@ contract Controller {
     }
 
     function decodeStructByIndex(uint256 index) public view returns (StructDefiner.MyStruct memory myStruct) {
+        // How is this represented in memory
         bytes memory encodedStruct = storageContract.getEncodedStructByIndex(index);
 
-        // I need to debug this assembly block to understand why my variables aren't being allocated
-        // in the same memory words as the ones being returned from the named return of this function
-        // 
-        // The rationale behing what's going on is the following
-        // 1 - Load the memory pointer to a variable
-        // 2 - Write the byte offsets from encoded struct into the same offsets of memory words
-        // 3 - Update the free memory pointer
-        // 
-        // This is probably not working because I'm writing a value a the memory address where my struct is stored
-        // and then, using the value of that field to write another value in memory + the offset of a word.
         assembly {
             myStruct := mload(0x40)
 
-            // mstore(add(freeMemoryPointer), )
             mstore(myStruct, mload(add(encodedStruct, 0x20)))
             mstore(add(myStruct, 0x20), mload(add(encodedStruct, 0x40)))
             mstore(add(myStruct, 0x40), mload(add(encodedStruct, 0x60)))
