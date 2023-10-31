68ed59afd4554678bfa3d20211415365b6accf93
Ian Guimarães <ianguimaraes31@gmail.com>
2023-10-31 17:12:34 -0300

    chore: update and rename ETHPool.sol to BikeShare.sol

--- @etherparty/src/ETHPool.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
-
-// 1. Deposit ETH and receive weekly rewards
-// 2. Must be able to withdraw at any time
-// 3. New rewards are deposited manually into the pool each week => only admin
-// 4. When the team deposits the rewards, only users with staked tokens should receive rewards
-contract ETHPool {
-}
